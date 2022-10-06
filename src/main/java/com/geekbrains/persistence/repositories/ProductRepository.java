4ee03b69b173c55a05b4ef8bdd441cc981d42b22
Ukolov1703 <Nissan_Skyline10>
2022-10-06 19:45:40 +0700

    dz5

--- src/main/java/com/geekbrains/persistence/repositories/ProductRepository.java
+++ src/main/java/com/geekbrains/persistence/repositories/ProductRepository.java
@@ -1,4 +1,17 @@
 package com.geekbrains.persistence.repositories;
 
-public class ProductRepository {
+import com.geekbrains.persistence.entities.Product;
+import java.util.List;
+
+public interface ProductRepository {
+
+    List<Product> findAll();
+
+    List<Product> findAllSortedByName();
+
+    Product findById(Long id);
+
+    void deleteById(Long id);
+
+    void saveOrUpdate(Product product);
 }
